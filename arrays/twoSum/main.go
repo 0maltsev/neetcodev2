b7c04482f92be14995b0c01e6dd3d822f7eb1701
Олег Мальцев <oyumaltsev@sberbank.ru>
2025-02-13 16:04:24 +0300

    ADD better twoSum

--- arrays/twoSum/main.go
+++ arrays/twoSum/main.go
@@ -3,27 +3,19 @@ package main
 import "fmt"
 
 func main(){
-	nums := []int{3,2,4}
-	target := 6
+	nums := []int{2,7,11,15}
+	target := 9
 
 	fmt.Println(twoSum(nums, target))
 }
 
 func twoSum(nums []int, target int) []int {
-	m := arrayToHashMap(nums)
+	m := make(map[int]int)
 	for index, element := range(nums) {
-		m[element] = m[element][1:]
-		if len(m[target-element]) != 0 {
-			return []int{index, m[target-element][0]}
+		if m[target - element] != 0 {
+			return []int{m[target - element]-1, index}
 		}
+		m[element] = index + 1
 	}
 	return []int{}
-}
-
-func arrayToHashMap(array []int) map[int][]int {
-	m := make(map[int][]int)
-	for index, element := range array {
-		m[element] = append(m[element], index)
-	}
-	return m
 }
