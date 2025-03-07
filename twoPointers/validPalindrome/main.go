f0da24311c14cee8675f159a2fcbf35b6bb0e2b6
Олег Мальцев <oyumaltsev@sberbank.ru>
2025-03-07 09:20:46 +0300

    ADD two pointers

---
+++ twoPointers/validPalindrome/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"unicode"
+)
+
+func main() {
+	s := "A man, a plan, a canal: Panama"
+	isPalindrome(s)
+}
+
+func isPalindrome(s string) bool {
+    l, r := 0, len(s)-1
+
+    for l < r {
+        for l < r && !isAlphaNum(rune(s[l])) {
+            l++
+        }
+        for r > l && !isAlphaNum(rune(s[r])) {
+            r--
+        }
+        if unicode.ToLower(rune(s[l])) != unicode.ToLower(rune(s[r])) {
+            return false
+        }
+        l++
+        r--
+    }
+    return true
+}
+
+func isAlphaNum(c rune) bool {
+    return unicode.IsLetter(c) || unicode.IsDigit(c)
+}
+
